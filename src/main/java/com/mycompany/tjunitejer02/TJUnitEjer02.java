bcfded45de547f4b25437500dfc0f2dc8307001e
Ingrid Valenzuela <ingrid2valenzuela@hotmail.com>
2023-11-07 22:56:35 -0500

    Primer commit

---
+++ src/main/java/com/mycompany/tjunitejer02/TJUnitEjer02.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tjunitejer02;
+
+/**
+ *
+ * @author 51986
+ */
+public class TJUnitEjer02 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
