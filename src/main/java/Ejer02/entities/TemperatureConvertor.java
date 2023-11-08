1a78e4eeb45b4add7d19db3e0e790beca1b9a17f
Ingrid Valenzuela <ingrid2valenzuela@hotmail.com>
2023-11-07 23:09:40 -0500

    Segundo commit

---
+++ src/main/java/Ejer02/entities/TemperatureConvertor.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ejer02.entities;
+
+/**
+ *
+ * @author 51986
+ */
+public class TemperatureConvertor {
+    private double temCelsius;
+    private double temKelvin;
+    private double temFahrenheit;
+    private double temRankine;
+    
+}
---
+++ src/main/java/Ejer02/services/TemperatureConvertorServices.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ejer02.services;
+
+import Ejer02.entities.TemperatureConvertor;
+
+/**
+ *
+ * @author 51986
+ */
+public class TemperatureConvertorServices {
+    
+    public double convertirACelsius(TemperatureConvertor tc){
+        return 0;
+    }
+    
+}
